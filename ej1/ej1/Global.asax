8be3cb58d2f5da69caffc52deceef515f7be7872
Martín Navarro <mn446@hotmail.com>
2017-10-08 23:22:43 +0200

    initial commit

---
+++ ej1/ej1/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ej1.MvcApplication" Language="C#" %>
---
+++ ej1/ej1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ej1.Startup))]
+namespace ej1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
